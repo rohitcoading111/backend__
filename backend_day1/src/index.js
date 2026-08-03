941e1083a86cfbc72906acea77b5c49384bfc755
rohitcoading111 <rp538220@gmail.com>
2026-08-03 17:22:41 +0530

    some practicing of get api

--- backend_day1/src/index.js
+++ backend_day1/src/index.js
@@ -6,6 +6,21 @@ app.get("/", (req, res) => {
     res.send("Hello Backend");
 });
 
+app.get("/about",(req,res)=>{
+     res.send("welcome to about page")
+})
+
+app.get("/practice",(req,res)=>{
+    res.send("i am practicing backend")
+})
+
+app.get("/collage",(req,res)=>{
+    res.send("Uou bca studene")
+})
+
+app.get("/skills",(req,res)=>{
+    res.send("i have skilled frontend developer")
+})
 
 const PORT = 3000;
 
