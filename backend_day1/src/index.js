7cccb575f0514a9b9713e0f3fd0bc7bd3567d8cd
rohitcoading111 <rp538220@gmail.com>
2026-08-03 16:57:31 +0530

    this is my first backend code

---
+++ backend_day1/src/index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello Backend");
+});
+
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
