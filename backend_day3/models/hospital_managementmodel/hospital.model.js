9ee28a67f60751e1627ddc2dd7ac7100011561b7
rohitcoading111 <rp538220@gmail.com>
2026-08-07 17:31:04 +0530

    hospital model has been created

--- backend_day3/models/hospital_managementmodel/hospital.model.js
+++ backend_day3/models/hospital_managementmodel/hospital.model.js
@@ -1,5 +1,25 @@
 import mongoose from "mongoose";
 
-const HospitalSchema = new mongoose.Schema({},{timestamps:true});
+const HospitalSchema = new mongoose.Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    address:{
+        type:String,
+        required:true
+    },
+    city:{
+        type:String,
+        required:true
+    },
+    pinCode:{
+        type:String,
+    },
+    specializedIn:[{
+        tyepe:String,
+    }],
+
+},{timestamps:true});
 
 export const Hospital = mongoose.model("Hospital", hospitalSchema);
