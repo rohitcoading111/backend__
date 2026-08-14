5750814a2f1d005486e7dd9a73a359d37353a8d1
rohitcoading111 <rp538220@gmail.com>
2026-08-14 22:17:25 +0530

    initializing error handling inn app.js file

--- backend_day4/src/index.js
+++ backend_day4/src/index.js
@@ -2,6 +2,7 @@
 
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import { app } from "./app.js"
 
 
 dotenv.config({
@@ -10,21 +11,12 @@ dotenv.config({
 
 connectDB()
 
-// import express from "express"
-// const app = express()
-
-// (async()=>{
-//     try {
-//        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
-//        app.on("error", (error)=> {
-//           console.log(error);
-//        })
-
-//        app.listen(process.env.PORT,()=>{
-//            console.log(`app is listining on port ${process.env.PORT}`);
-           
-//        })
-//     } catch (error) {
-//         console.error("ERROR",error)
-//     }
-// })()
+.then(()=>{
+    app.listen(process.env.PORT || 8000 , ()=>{
+        console.log(`server is running on port => ${process.env.PORT}`);
+    })
+})
+.catch((err)=>{
+   console.log("mongo db connection error",err);
+   
+})
