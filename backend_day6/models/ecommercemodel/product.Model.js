852a7ce6102d38c032ccecdb4752b921718c3af1
rohitcoading111 <rp538220@gmail.com>
2026-08-22 00:08:54 +0530

    product  schema has been completed

---
+++ backend_day6/models/ecommercemodel/product.Model.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+     name:{
+        type:String,
+        required:true
+     },
+     category:{
+        type:String,
+        required:true
+     },
+     price:{
+        type:Number,
+        required:true
+     },
+     image:{
+        type:String,
+     }
+},{timestamps:true})
+
+
+export const Product = mongoose.model("Product",productSchema)
