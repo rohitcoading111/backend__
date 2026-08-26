88afc135789e7be0db21d4519fea6c715b9ae8b0
rohitcoading111 <rp538220@gmail.com>
2026-08-27 00:48:23 +0530

    using /file path

--- backend_day8/src/app.js
+++ backend_day8/src/app.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const app = express();
-
+const file = require("./routes/file.route")
 app.use(express.json());
 
 app.get("/",(req,res)=>{
     res.send("backend running successfully");
 });
+
+app.use("/file",file)
 
 module.exports = app;
