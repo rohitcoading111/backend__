04d608195cf9c2ea061e8ef5fc0f3c9af6422187
rohitcoading111 <rp538220@gmail.com>
2026-08-29 21:53:24 +0530

    some basic routes has been setuped

--- backend_day9/frontend/src/App.jsx
+++ backend_day9/frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 const App = () => {
   return (
-    <div>
-      
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<h1>hellow</h1>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
