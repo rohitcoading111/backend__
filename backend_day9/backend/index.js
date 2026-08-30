1bcdd405ecb9b6203a2cf72db5f424d09591e91a
rohitcoading111 <rp538220@gmail.com>
2026-08-30 17:01:46 +0530

    post creataion and integration part successfully completed

--- backend_day9/backend/index.js
+++ backend_day9/backend/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-
+const cors = require("cors");
 const express = require("express");
 const connectDB = require("./src/db/db");
 const postRoutes = require("./src/routes/post.route");
@@ -7,7 +7,7 @@ const postRoutes = require("./src/routes/post.route");
 const app = express();
 
 connectDB();
-
+app.use(cors());
 app.use(express.json());
 
 app.use("/posts", postRoutes);
--- backend_day9/frontend/src/pages/CreatePost.jsx
+++ backend_day9/frontend/src/pages/CreatePost.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 
 const CreatePost = () => {
+
+const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    try {
+        const response = await fetch("http://localhost:3000/posts", {
+            method: "POST",
+            body: formData
+        });
+
+        const data = await response.json();
+
+        console.log(data);
+    } catch (error) {
+        console.log(error);
+    }
+};
+
   return (
     <div>
       <section className='create-post-section'>
           <h1>Create post </h1>
-          <form>
+          <form onSubmit={handleSubmit}>
             <input type="file" name='image' accept='image/*' />
             <input type="text" name="caption" placeholder='enter caption' required />
             <button type='submit'>Publish Post</button>
