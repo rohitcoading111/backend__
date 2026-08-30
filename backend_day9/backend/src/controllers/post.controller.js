06288561ffd3f211b4670d0f05189a79a73b360c
rohitcoading111 <rp538220@gmail.com>
2026-08-30 16:52:30 +0530

    controller has been successfully created

--- backend_day9/backend/src/controllers/post.controller.js
+++ backend_day9/backend/src/controllers/post.controller.js
@@ -2,8 +2,6 @@ const Post = require("../models/post.model");
 
 const createPost = async (req, res) => {
     try {
-        console.log("BODY:", req.body);
-        console.log("FILE:", req.file);
 
         const { caption } = req.body;
 
