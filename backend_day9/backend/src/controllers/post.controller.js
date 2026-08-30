02360dbe83f527912b03192db8ff3ef73802c949
rohitcoading111 <rp538220@gmail.com>
2026-08-30 11:58:42 +0530

    post controller has been created

---
+++ backend_day9/backend/src/controllers/post.controller.js
@@ -0,0 +1,47 @@
+const Post = require("../models/post.model");
+
+const createPost = async (req, res) => {
+    try {
+        const { caption } = req.body;
+
+        const post = await Post.create({
+            image: req.file.path,
+            caption
+        });
+
+        res.status(201).json({
+            message: "Post created successfully",
+            post
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Failed to create post",
+            error: error.message
+        });
+    }
+};
+
+
+const getPosts = async (req, res) => {
+    try {
+        const posts = await Post.find();
+
+        res.status(200).json({
+            message: "Posts fetched successfully",
+            posts
+        });
+
+    } catch (error) {
+        res.status(500).json({
+            message: "Failed to fetch posts",
+            error: error.message
+        });
+    }
+};
+
+
+module.exports = {
+    createPost,
+    getPosts
+};
