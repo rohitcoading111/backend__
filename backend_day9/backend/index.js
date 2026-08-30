025ae3c0e7101c2c0655b6c2acf6e0f65e1f7882
rohitcoading111 <rp538220@gmail.com>
2026-08-30 12:02:42 +0530

    route successfully created

--- backend_day9/backend/index.js
+++ backend_day9/backend/index.js
@@ -1,11 +1,13 @@
-const express = require('express'); 
-const app = express(); 
-const PORT = 3000;
+const express = require("express");
 
-app.get('/', (req, res) => { 
-    res.send('Server is running successfully'); 
-});
+const app = express();
 
-app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}`);
+const postRoutes = require("./routes/post.route");
+
+app.use(express.json());
+
+app.use("/posts", postRoutes);
+
+app.listen(3000, () => {
+    console.log("Server running on port 3000");
 });
