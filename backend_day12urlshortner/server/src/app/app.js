ef0dfa5acde1e814ea4449042bc1123e57bbe1ba
rohitcoading111 <rp538220@gmail.com>
2026-09-13 23:00:20 +0530

    route has been successfully created

--- backend_day12urlshortner/server/src/app/app.js
+++ backend_day12urlshortner/server/src/app/app.js
@@ -1,6 +1,7 @@
 import express from "express"
 import urlRoutes from "../routes/url.routes.js"
 const app = express();
+app.use(express.json());
 
 app.use("/api/url",urlRoutes)
 export default app;
--- backend_day12urlshortner/server/src/routes/url.routes.js
+++ backend_day12urlshortner/server/src/routes/url.routes.js
@@ -1,6 +1,42 @@
 import express from "express"
 import generateCode from "../utils/generateCode.js"
+import { Url } from "../models/url.model.js";
+
 const router = express.Router();
+router.post("/",async function(req,res){
+   const {url} = req.body;
+   if(!url){
+    return res.status(400).json({
+        error:"url is requireed"
+    })
+   }
+
+   if(url.startsWith("http://")==false && url.startsWith("https://")==false){
+    return res.status(400).json({
+        error: "url is invalid"
+    })
+   }
+
+   if(url.length>2048){
+    return resizeBy.status(400).json({
+        error:"url is too long"
+    })
+   }
+
+   const code  = generateCode();
+   const newUrl = await Url.create({
+    originalUrl:url,
+    shortCode:code,
+   });
+   return res.status(201).json({
+     message:"url shorten successfully",
+     data:{
+        originalUrl:newUrl.originalUrl,
+        shortCode:newUrl.shortCode
+     }
+   })
+})
+
 
 
 
