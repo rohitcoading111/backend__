db324c860278711ddfc295f477219ab9e8a79206
rohitcoading111 <rp538220@gmail.com>
2026-09-13 22:31:50 +0530

    url routing has been creating

--- backend_day12urlshortner/server/src/app/app.js
+++ backend_day12urlshortner/server/src/app/app.js
@@ -1,6 +1,6 @@
 import express from "express"
-
+import urlRoutes from "../routes/url.routes.js"
 const app = express();
 
-
+app.use("/api/url",urlRoutes)
 export default app;
---
+++ backend_day12urlshortner/server/src/routes/url.routes.js
@@ -0,0 +1,7 @@
+import express from "express"
+import generateCode from "../utils/generateCode.js"
+const router = express.Router();
+
+
+
+export default router;
