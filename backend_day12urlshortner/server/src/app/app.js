04cc0083c59b728c75857b9af4970b26b30d50a3
rohitcoading111 <rp538220@gmail.com>
2026-09-13 23:13:41 +0530

    redirecting has been successfully

--- backend_day12urlshortner/server/src/app/app.js
+++ backend_day12urlshortner/server/src/app/app.js
@@ -1,7 +1,25 @@
 import express from "express"
 import urlRoutes from "../routes/url.routes.js"
+import { Url } from "../models/url.model.js";
 const app = express();
 app.use(express.json());
 
 app.use("/api/url",urlRoutes)
+
+app.get("/:code",async function(req,res){
+    const {code}  = req.params
+
+    const url = await Url.findOne({
+        shortCode: code
+    })
+
+    if(!url){
+        return res.status(404).json({
+         error:"url not found"
+        }
+      )
+    }
+
+    res.redirect(302, url.originalUrl)
+})
 export default app;
