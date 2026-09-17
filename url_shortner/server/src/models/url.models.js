bd849550295cc2f14f6f82be5d862871e4027076
rohitcoading111 <rp538220@gmail.com>
2026-09-17 13:56:12 +0530

    url schema has been created

---
+++ url_shortner/server/src/models/url.models.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose"
+
+const urlSchema = new mongoose.Schema({
+    originalUrl :{
+        type:String,
+        required:true
+    },
+    shortCode:{
+        type:String,
+        required:true
+    },
+    clicks:{
+        type:Number,
+        default:0
+    }
+},{timestamps:true})
+
+const urlmodel = mongoose.model("urlmodel",urlSchema);
+
+export default urlmodel;
