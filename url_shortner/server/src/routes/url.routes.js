ba887b1a616b1f393ce3903d3d7456c681c10a8c
rohitcoading111 <rp538220@gmail.com>
2026-09-17 14:05:28 +0530

    url route has been updated

--- url_shortner/server/src/routes/url.routes.js
+++ url_shortner/server/src/routes/url.routes.js
@@ -2,13 +2,6 @@ import express from "express"
 
 const router = express.Router();
 
-router.post('/',async function(req,res){
-    const {url} = req.body
-    if(!url){
-        return res.status(400).json({
-            message:"url is required"
-        });
-    }
-})
+router.post('/',async function(req,res){})
 
 export default router;
