2ae827473ae46c0bc05cd1ffa543e9e7ff243b26
rohitcoading111 <rp538220@gmail.com>
2026-09-17 14:35:27 +0530

    shortCode generator has been completed

---
+++ url_shortner/server/src/utils/shortCode.js
@@ -0,0 +1,13 @@
+const generateCode = ()=>{
+  const characters = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ123456789"
+  let shortCode = ""
+  for(let i = 0; i<6; i++){
+      shortCode += characters.charAt(Math.floor(Math.random()*characters.length))
+  }
+  console.log(shortCode);
+  
+  return shortCode;
+  
+}
+generateCode();
+export default generateCode;
