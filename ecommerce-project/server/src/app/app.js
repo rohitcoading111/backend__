9fe7ce2961a8bd472217fa07b4e9ebaed3d83976
rohitcoading111 <rp538220@gmail.com>
2026-09-23 15:15:01 +0530

    basic router has been setuped

--- ecommerce-project/server/src/app/app.js
+++ ecommerce-project/server/src/app/app.js
@@ -1,7 +1,7 @@
 import express from "express"
-
+import authRoute from "../routes/auth.routes.js";
 
 const app = express();
 app.use(express.json())
-
+app.use("/api/auth",authRoute)
 export default app;
