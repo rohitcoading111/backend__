98b80491c60d2868616bf922fac1dd0018038c15
rohitcoading111 <rp538220@gmail.com>
2026-09-23 23:02:44 +0530

    login validator has been imported on a auth route file

--- ecommerce-project/server/src/routes/auth.routes.js
+++ ecommerce-project/server/src/routes/auth.routes.js
@@ -1,8 +1,9 @@
 import express from "express"
-import {registerValidation} from "../validators/auth.validator.js"
+import {registerValidation,loginValidator} from "../validators/auth.validator.js"
 import {registerController} from "../controllers/auth.controllers.js"
 const router = express.Router()
 
 router.post("/register" ,registerValidation,registerController)
+router.post("/login",loginValidator)
 
 export default router
