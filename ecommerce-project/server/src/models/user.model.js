1a1d67dee2f39ac5fd951063d15e0db1e55a96ae
rohitcoading111 <rp538220@gmail.com>
2026-09-23 14:59:48 +0530

    userModel has been successfully created

---
+++ ecommerce-project/server/src/models/user.model.js
@@ -0,0 +1,27 @@
+import mongoose from "mongoose"
+
+const userSchema = mongoose.Schema({
+    name:{
+        type:String,
+        required:true,
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true,
+        lowercase: true
+    },
+    password:{
+        type:String,
+        required:true,
+        minLength:6,
+        maxLength:50
+    },
+    
+},{
+        timestamps:true
+    })
+
+const userModel = mongoose.model("userModel",userSchema)
+
+export default userModel
