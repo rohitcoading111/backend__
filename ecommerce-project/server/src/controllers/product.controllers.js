1e3cc00fed6646c795a6e1d72203e5703bbedf9d
rohitcoading111 <rp538220@gmail.com>
2026-09-26 06:32:43 +0530

    get single product api has been completed

--- ecommerce-project/server/src/controllers/product.controllers.js
+++ ecommerce-project/server/src/controllers/product.controllers.js
@@ -45,3 +45,29 @@ export const getAllProductsController = async (req, res) => {
         });
     }
 };
+
+export const getSingleProductController = async (req, res) => {
+    try {
+        const { id } = req.params;
+
+        const product = await productModel.findById(id);
+
+        if (!product) {
+            return res.status(404).json({
+                message: "Product not found"
+            });
+        }
+
+        return res.status(200).json({
+            message: "Product fetched successfully",
+            data: product
+        });
+
+    } catch (error) {
+        console.log("GET SINGLE PRODUCT ERROR:", error);
+
+        return res.status(500).json({
+            message: "Internal server error"
+        });
+    }
+};
--- ecommerce-project/server/src/routes/product.routes.js
+++ ecommerce-project/server/src/routes/product.routes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { productValidation } from "../validators/product.validator.js";
-import { createProductController,getAllProductsController } from "../controllers/product.controllers.js";
+import { createProductController,getAllProductsController,getSingleProductController } from "../controllers/product.controllers.js";
 import { authMiddleware,sellerMiddleware } from "../middleware/auth.middleware.js";
 import upload from "../middleware/upload.middleware.js";
 
@@ -8,5 +8,6 @@ const router = express.Router();
 
 router.post( "/products",authMiddleware,sellerMiddleware,upload.single("image"),productValidation,createProductController);
 router.get("/allproducts", getAllProductsController);
+router.get("/products/:id",getSingleProductController)
 
 export default router;
